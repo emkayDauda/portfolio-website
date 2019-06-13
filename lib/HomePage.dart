676c3b6fa2d1aafe93e4d02866955aeb25c3e357
Maaruf Dauda <mkdauda.work@gmail.com>
2019-06-13 16:13:41 +0100

    reformat code

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,36 +1,39 @@
 import 'package:flutter_web/material.dart';
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
 
   final String title;
 
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-   
     return Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-            _buildMenuBar(),
-        ],
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        _buildMenuBar(),
+      ],
     );
   }
-}
 
-Widget _buildMenuBar(){
+  Widget _buildMenuBar() {
     return Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-            _buildMenuItem('About Me'),
-            _buildMenuItem('Experience'),
-            _buildMenuItem('Personal Projects')
-        ],
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        _buildMenuItem('About Me'),
+        _buildMenuItem('Experience'),
+        _buildMenuItem('Personal Projects')
+      ],
     );
-}
+  }
 
-Widget _buildMenuItem(String title){
+  Widget _buildMenuItem(String title) {
     return Text(
-        title,
-        // style: Theme.of(context).textTheme.body2,
-        );
+      title,
+      style: Theme.of(context).textTheme.body2,
+    );
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'emkayDauda Portfolio'),
+      home: HomePage(title: 'emkayDauda Portfolio'),
     );
   }
 }
