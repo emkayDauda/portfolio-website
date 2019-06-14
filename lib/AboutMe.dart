3d09b426920b2f76bb9de5ab801c671ebaed20a5
Maaruf Dauda <mkdauda.work@gmail.com>
2019-06-14 09:37:56 +0100

    Create column sections for About me

--- lib/AboutMe.dart
+++ lib/AboutMe.dart
@@ -1,11 +1,45 @@
 import 'package:flutter_web/material.dart';
 
-class AboutMe extends StatelessWidget{
-    @override
-    Widget build(BuildContext context) {
-      return Container(
-          height: 200.0,
-          color: Colors.amber[600],
-      );
-    }
+import 'HomePage.dart';
+
+class AboutMe extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200.0,
+      color: Colors.amber[600],
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                HomePageState().buildTitleText(
+                  'A few details about me...',
+                  Colors.white,
+                  size: 35.0,
+                )
+              ],
+            ),
+          ),
+          Expanded(
+                      child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                HomePageState().buildTitleText(
+                  'What I look like!',
+                  Colors.white,
+                  size: 25.0,
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
 }
