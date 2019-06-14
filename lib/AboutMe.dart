643435e6444fcf08f925f875ece8e870d730d6b4
Maaruf Dauda <mkdauda.work@gmail.com>
2019-06-14 09:05:56 +0100

    Remove container width to fit screen

--- lib/AboutMe.dart
+++ lib/AboutMe.dart
@@ -4,7 +4,6 @@ class AboutMe extends StatelessWidget{
     @override
     Widget build(BuildContext context) {
       return Container(
-          width: 400.0,
           height: 200.0,
           color: Colors.amber[600],
       );
