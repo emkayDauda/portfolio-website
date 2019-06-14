d09a45584f10b87e617005474a83ee2c52deca8c
Maaruf Dauda <mkdauda.work@gmail.com>
2019-06-14 09:01:33 +0100

    Add container to AboutMe for background color

--- lib/AboutMe.dart
+++ lib/AboutMe.dart
@@ -1,1 +1,12 @@
 import 'package:flutter_web/material.dart';
+
+class AboutMe extends StatelessWidget{
+    @override
+    Widget build(BuildContext context) {
+      return Container(
+          width: 400.0,
+          height: 200.0,
+          color: Colors.amber[600],
+      );
+    }
+}
